fefe1e6a442fca8c9d699bd7f3ba835c2e88fe2f
Jérémie Drouet <jeremie.drouet@gmail.com>
2022-11-15 11:09:42 +0000

    feat(movie): create command for external ids
    Signed-off-by: Jérémie Drouet <jeremie.drouet@gmail.com>

---
+++ src/movie/external_ids.rs
@@ -0,0 +1,125 @@
+use serde::{Deserialize, Serialize};
+use std::borrow::Cow;
+
+/// Command to get similar movies to a movie
+///
+/// ```rust
+/// use tmdb_api::prelude::Command;
+/// use tmdb_api::Client;
+/// use tmdb_api::movie::external_ids::MovieExternalIds;
+///
+/// #[tokio::main]
+/// async fn main() {
+///     let client = Client::new("this-is-my-secret-token".into());
+///     let cmd = MovieExternalIds::new(1);
+///     let result = cmd.execute(&client).await;
+///     match result {
+///         Ok(res) => println!("found: {:#?}", res),
+///         Err(err) => eprintln!("error: {:?}", err),
+///     };
+/// }
+/// ```
+#[derive(Clone, Debug, Default)]
+pub struct MovieExternalIds {
+    /// ID of the movie
+    pub movie_id: u64,
+}
+
+impl MovieExternalIds {
+    pub fn new(movie_id: u64) -> Self {
+        Self { movie_id }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct MovieExternalIdsResult {
+    pub id: u64,
+    pub imdb_id: Option<String>,
+    pub facebook_id: Option<String>,
+    pub instagram_id: Option<String>,
+    pub twitter_id: Option<String>,
+}
+
+impl crate::prelude::Command for MovieExternalIds {
+    type Output = MovieExternalIdsResult;
+
+    fn path(&self) -> Cow<'static, str> {
+        Cow::Owned(format!("/movie/{}/external_ids", self.movie_id))
+    }
+
+    fn params(&self) -> Vec<(&'static str, Cow<'_, str>)> {
+        Vec::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::MovieExternalIds;
+    use crate::prelude::Command;
+    use crate::Client;
+    use mockito::{mock, Matcher};
+
+    #[tokio::test]
+    async fn it_works() {
+        let client = Client::new("secret".into()).with_base_url(mockito::server_url());
+        let cmd = MovieExternalIds::new(335984);
+
+        let _m = mock("GET", "/movie/335984/external_ids")
+            .match_query(Matcher::UrlEncoded("api_key".into(), "secret".into()))
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .with_body(include_str!("../../assets/movie-external-ids-success.json"))
+            .create();
+        let result = cmd.execute(&client).await.unwrap();
+        assert_eq!(result.id, 335984);
+    }
+
+    #[tokio::test]
+    async fn invalid_api_key() {
+        let client = Client::new("secret".into()).with_base_url(mockito::server_url());
+        let cmd = MovieExternalIds::new(42);
+
+        let _m = mock("GET", "/movie/42/external_ids")
+            .match_query(Matcher::UrlEncoded("api_key".into(), "secret".into()))
+            .with_status(401)
+            .with_header("content-type", "application/json")
+            .with_body(include_str!("../../assets/invalid-api-key.json"))
+            .create();
+        let err = cmd.execute(&client).await.unwrap_err();
+        let server_err = err.as_server_error().unwrap();
+        assert_eq!(server_err.body.as_other_error().unwrap().status_code, 7);
+    }
+
+    #[tokio::test]
+    async fn resource_not_found() {
+        let client = Client::new("secret".into()).with_base_url(mockito::server_url());
+        let cmd = MovieExternalIds::new(42);
+
+        let _m = mock("GET", "/movie/42/external_ids")
+            .match_query(Matcher::UrlEncoded("api_key".into(), "secret".into()))
+            .with_status(404)
+            .with_header("content-type", "application/json")
+            .with_body(include_str!("../../assets/resource-not-found.json"))
+            .create();
+        let err = cmd.execute(&client).await.unwrap_err();
+        let server_err = err.as_server_error().unwrap();
+        assert_eq!(server_err.body.as_other_error().unwrap().status_code, 34);
+    }
+}
+
+#[cfg(all(test, feature = "integration"))]
+mod integration_tests {
+    use super::MovieExternalIds;
+    use crate::prelude::Command;
+    use crate::Client;
+
+    #[tokio::test]
+    async fn execute() {
+        let secret = std::env::var("TMDB_TOKEN_V3").unwrap();
+        let client = Client::new(secret);
+        let cmd = MovieExternalIds::new(335984);
+
+        let result = cmd.execute(&client).await.unwrap();
+        assert_eq!(result.id, 335984);
+    }
+}
