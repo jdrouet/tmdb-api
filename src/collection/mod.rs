a9028a999592a42d578e7fd8235c8b1a74bc9fd2
AGMBK <109393672+agmbk@users.noreply.github.com>
2024-01-24 05:30:09 +0100

    fix(collection): nullable overview field

--- src/collection/mod.rs
+++ src/collection/mod.rs
@@ -5,7 +5,7 @@ pub mod details;
 pub struct CollectionBase {
     pub id: u64,
     pub name: String,
-    pub overview: String,
+    pub overview: Option<String>,
     pub poster_path: Option<String>,
     pub backdrop_path: Option<String>,
 }
