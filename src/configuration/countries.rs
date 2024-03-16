15ad3cbb941316e07253eec6dbef154ee46326cf
AGMBK <109393672+agmbk@users.noreply.github.com>
2024-03-16 01:38:32 +0100

    feat: configuration
    Implements the configuration endpoints
    https://developer.themoviedb.org/reference/configuration-countries
    https://developer.themoviedb.org/reference/configuration-jobs
    https://developer.themoviedb.org/reference/configuration-languages

---
+++ src/configuration/countries.rs
@@ -0,0 +1,160 @@
+//! https://developer.themoviedb.org/reference/configuration-countries
+
+use std::borrow::Cow;
+
+/// Get a list of all countries
+///
+/// ```rust
+/// use tmdb_api::prelude::Command;
+/// use tmdb_api::Client;
+/// use tmdb_api::configuration::countries::Countries;
+///
+/// #[tokio::main]
+/// async fn main() {
+///     let client = Client::new("this-is-my-secret-token".into());
+///     let result = Countries::default().execute(&client).await;
+///     match result {
+///         Ok(res) => println!("found: {res:#?}"),
+///         Err(err) => eprintln!("error: {err:?}"),
+///     };
+/// }
+/// ```
+#[derive(Clone, Debug, Default)]
+pub struct Countries {
+    language: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CountriesResult {
+    pub iso_3166_1: String,
+    pub english_name: String,
+    pub native_name: String,
+}
+
+impl Countries {
+    pub fn new() -> Self {
+        Self { language: None }
+    }
+
+    pub fn with_language(mut self, value: Option<String>) -> Self {
+        self.language = value;
+        self
+    }
+}
+
+impl crate::prelude::Command for Countries {
+    type Output = Vec<CountriesResult>;
+
+    fn path(&self) -> Cow<'static, str> {
+        Cow::Owned("/configuration/countries".to_owned())
+    }
+
+    fn params(&self) -> Vec<(&'static str, Cow<'_, str>)> {
+        if let Some(ref language) = self.language {
+            vec![("language", Cow::Borrowed(language))]
+        } else {
+            Vec::new()
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use mockito::Matcher;
+
+    use crate::Client;
+    use crate::prelude::Command;
+
+    use super::Countries;
+
+    #[tokio::test]
+    async fn it_works() {
+        let mut server = mockito::Server::new_async().await;
+        let client = Client::builder()
+            .with_api_key("secret".into())
+            .with_base_url(server.url())
+            .build()
+            .unwrap();
+
+        let _m = server
+            .mock("GET", "/configuration/countries")
+            .match_query(Matcher::UrlEncoded("api_key".into(), "secret".into()))
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .with_body(include_str!("../../assets/configuration-countries.json"))
+            .create_async()
+            .await;
+
+        let result = Countries::default().execute(&client).await.unwrap();
+        assert!(!result.is_empty());
+    }
+
+    #[tokio::test]
+    async fn invalid_api_key() {
+        let mut server = mockito::Server::new_async().await;
+        let client = Client::builder()
+            .with_api_key("secret".into())
+            .with_base_url(server.url())
+            .build()
+            .unwrap();
+
+        let _m = server
+            .mock("GET", "/configuration/countries")
+            .match_query(Matcher::UrlEncoded("api_key".into(), "secret".into()))
+            .with_status(401)
+            .with_header("content-type", "application/json")
+            .with_body(include_str!("../../assets/invalid-api-key.json"))
+            .create_async()
+            .await;
+
+        let err = Countries::default()
+            .execute(&client)
+            .await
+            .unwrap_err();
+        let server_err = err.as_server_error().unwrap();
+        assert_eq!(server_err.body.as_other_error().unwrap().status_code, 7);
+    }
+
+    #[tokio::test]
+    async fn resource_not_found() {
+        let mut server = mockito::Server::new_async().await;
+        let client = Client::builder()
+            .with_api_key("secret".into())
+            .with_base_url(server.url())
+            .build()
+            .unwrap();
+
+        let _m = server
+            .mock("GET", "/configuration/countries")
+            .match_query(Matcher::UrlEncoded("api_key".into(), "secret".into()))
+            .with_status(404)
+            .with_header("content-type", "application/json")
+            .with_body(include_str!("../../assets/resource-not-found.json"))
+            .create_async()
+            .await;
+
+        let err = Countries::default()
+            .execute(&client)
+            .await
+            .unwrap_err();
+        let server_err = err.as_server_error().unwrap();
+        assert_eq!(server_err.body.as_other_error().unwrap().status_code, 34);
+    }
+}
+
+#[cfg(all(test, feature = "integration"))]
+mod integration_tests {
+    use crate::Client;
+    use crate::prelude::Command;
+
+    use super::Countries;
+
+    #[tokio::test]
+    async fn execute() {
+        let secret = std::env::var("TMDB_TOKEN_V3").unwrap();
+        let client = Client::new(secret);
+
+        let result = Countries::default().execute(&client).await.unwrap();
+        assert!(!result.is_empty());
+    }
+}
---
+++ src/configuration/languages.rs
@@ -0,0 +1,150 @@
+//! https://developer.themoviedb.org/reference/configuration-languages
+
+use std::borrow::Cow;
+
+/// Get a list of all languages
+///
+/// ```rust
+/// use tmdb_api::prelude::Command;
+/// use tmdb_api::Client;
+/// use tmdb_api::configuration::languages::Languages;
+///
+/// #[tokio::main]
+/// async fn main() {
+///     let client = Client::new("this-is-my-secret-token".into());
+///     let result = Languages::default().execute(&client).await;
+///     match result {
+///         Ok(res) => println!("found: {res:#?}"),
+///         Err(err) => eprintln!("error: {err:?}"),
+///     };
+/// }
+/// ```
+#[derive(Clone, Debug, Default)]
+pub struct Languages {}
+
+#[derive(Debug, Deserialize)]
+pub struct LanguagesResult {
+    pub iso_639_1: String,
+    pub english_name: String,
+    /// Can be an empty string
+    pub name: String,
+}
+
+impl Languages {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl crate::prelude::Command for Languages {
+    type Output = Vec<LanguagesResult>;
+
+    fn path(&self) -> Cow<'static, str> {
+        Cow::Owned("/configuration/languages".to_owned())
+    }
+
+    fn params(&self) -> Vec<(&'static str, Cow<'_, str>)> {
+        Vec::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use mockito::Matcher;
+
+    use crate::Client;
+    use crate::prelude::Command;
+
+    use super::Languages;
+
+    #[tokio::test]
+    async fn it_works() {
+        let mut server = mockito::Server::new_async().await;
+        let client = Client::builder()
+            .with_api_key("secret".into())
+            .with_base_url(server.url())
+            .build()
+            .unwrap();
+
+        let _m = server
+            .mock("GET", "/configuration/languages")
+            .match_query(Matcher::UrlEncoded("api_key".into(), "secret".into()))
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .with_body(include_str!("../../assets/configuration-languages.json"))
+            .create_async()
+            .await;
+
+        let result = Languages::default().execute(&client).await.unwrap();
+        assert!(!result.is_empty());
+    }
+
+    #[tokio::test]
+    async fn invalid_api_key() {
+        let mut server = mockito::Server::new_async().await;
+        let client = Client::builder()
+            .with_api_key("secret".into())
+            .with_base_url(server.url())
+            .build()
+            .unwrap();
+
+        let _m = server
+            .mock("GET", "/configuration/languages")
+            .match_query(Matcher::UrlEncoded("api_key".into(), "secret".into()))
+            .with_status(401)
+            .with_header("content-type", "application/json")
+            .with_body(include_str!("../../assets/invalid-api-key.json"))
+            .create_async()
+            .await;
+
+        let err = Languages::default()
+            .execute(&client)
+            .await
+            .unwrap_err();
+        let server_err = err.as_server_error().unwrap();
+        assert_eq!(server_err.body.as_other_error().unwrap().status_code, 7);
+    }
+
+    #[tokio::test]
+    async fn resource_not_found() {
+        let mut server = mockito::Server::new_async().await;
+        let client = Client::builder()
+            .with_api_key("secret".into())
+            .with_base_url(server.url())
+            .build()
+            .unwrap();
+
+        let _m = server
+            .mock("GET", "/configuration/languages")
+            .match_query(Matcher::UrlEncoded("api_key".into(), "secret".into()))
+            .with_status(404)
+            .with_header("content-type", "application/json")
+            .with_body(include_str!("../../assets/resource-not-found.json"))
+            .create_async()
+            .await;
+
+        let err = Languages::default()
+            .execute(&client)
+            .await
+            .unwrap_err();
+        let server_err = err.as_server_error().unwrap();
+        assert_eq!(server_err.body.as_other_error().unwrap().status_code, 34);
+    }
+}
+
+#[cfg(all(test, feature = "integration"))]
+mod integration_tests {
+    use crate::Client;
+    use crate::prelude::Command;
+
+    use super::Languages;
+
+    #[tokio::test]
+    async fn execute() {
+        let secret = std::env::var("TMDB_TOKEN_V3").unwrap();
+        let client = Client::new(secret);
+
+        let result = Languages::default().execute(&client).await.unwrap();
+        assert!(!result.is_empty());
+    }
+}
